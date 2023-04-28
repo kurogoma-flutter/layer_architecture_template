15022007629b07d4b04b1fa2a002adbf44a6d69d
Kurogoma939 <kan.sss.939@gmail.com>
2023-04-28 10:40:00 +0900

    API実装（仮）

---
+++ lib/infrastructure/data_source/api_rest_client.dart
@@ -0,0 +1,19 @@
+// import 'package:retrofit/retrofit.dart';
+
+// class ApiRestClient {
+//   @POST('/favorite')
+//   Future<SampleResponseBody?> postMylistContentsUpdate(
+//     @Body() MylistContentsUpdateRequestBody mylistContentsUpdateRequestBody,
+//   );
+// }
+
+// class SampleResponseBody {
+//   SampleResponseBody({
+//     this.status,
+//     this.message,
+//     this.data,
+//   });
+//   final String? status;
+//   final String? message;
+//   final String? data;
+// }
