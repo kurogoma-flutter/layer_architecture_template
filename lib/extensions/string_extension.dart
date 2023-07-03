21d2748b4ca0d12bc988808564378de2ef8cbd2d
Kurogoma939 <kan.sss.939@gmail.com>
2023-07-04 00:28:08 +0900

    string extension追記

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -1,11 +1,17 @@
 import 'dart:math';
 
 extension StringExtension on String? {
+  // 日付変換
   DateTime toDate() => DateTime.parse(this!);
+  // 数値変換
   int toInt() => int.parse(this!);
+  // 数値変換（double）
   double toDouble() => double.parse(this!);
+  // URLかどうか判定
   bool get isUrl => RegExp(r'https?://[a-zA-Z0-9\-%_/=&?.]+').hasMatch(this!);
+  // IDかどうか判定
   bool get isId => RegExp(r'[a-zA-Z0-9\-%_/=&?.]+').hasMatch(this!);
+  // メールアドレスかどうか判定
   bool get isEmail =>
       RegExp('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}').hasMatch(this!);
 
@@ -13,7 +19,12 @@ extension StringExtension on String? {
   bool get isPassword =>
       RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9.?/-]{8,}$')
           .hasMatch(this!);
-
+  // 電話番号かどうか判定
+  bool get isPhoneNumber =>
+      RegExp(r'^[0-9]{2,4}-[0-9]{2,4}-[0-9]{3,4}$').hasMatch(this!);
+  // 郵便番号かどうか判定
+  bool get isPostalCode => RegExp(r'^[0-9]{3}-[0-9]{4}$').hasMatch(this!);
+  // 文字列が空かどうか判定
   bool get isNullOrEmpty {
     return this == null || this!.isEmpty;
   }
