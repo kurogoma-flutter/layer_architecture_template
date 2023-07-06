db833c0b4036f429e49cc3a7d610370bec9903ce
Kurogoma939 <kan.sss.939@gmail.com>
2023-07-06 14:56:31 +0900

    isarサンプルコード追加

---
+++ lib/isar_instance.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'infrastructure/data_source/isar/todo/todo_collection.dart';
+
+class IsarInstance {
+  static final IsarInstance _singleton = IsarInstance._internal();
+
+  Isar? _isar;
+
+  IsarInstance._internal();
+
+  factory IsarInstance() {
+    return _singleton;
+  }
+
+  Future<void> init() async {
+    final dir = await getApplicationSupportDirectory();
+    _isar = await Isar.open(
+      [
+        TodoSchema,
+      ],
+      directory: kIsWeb ? '' : dir.path,
+      inspector: true,
+    );
+  }
+
+  Isar get isar => _isar!;
+
+  IsarCollection<Todo> get todos => _isar!.collection<Todo>();
+}
